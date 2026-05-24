2683a561b9935c07c262d5b154a6219ee8eb3455
OMAR MOHAMMED OMAR AL-AMODI <TP084080@mail.apu.edu.my>
2026-05-25 01:55:04 +0800

    chore: configure Firebase Android test setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/test.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:nutriscan/firebase_options.dart';
 import 'package:nutriscan/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  setupFirebaseCoreMocks();
+
+  setUpAll(() async {
+    try {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.web,
+      );
+    } on FirebaseException catch (error) {
+      if (error.code != 'duplicate-app') rethrow;
+    }
+  });
+
   testWidgets('NutriScan home screen loads', (tester) async {
     await tester.pumpWidget(const NutriScanApp());
 
-    expect(find.text('Choose better food today'), findsOneWidget);
-    expect(find.text('Scan Food'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 }
